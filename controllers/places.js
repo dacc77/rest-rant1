db276902c7b2cd58287431717284292170781f26
dacc77 <123707776+dacc77@users.noreply.github.com>
2023-11-20 17:50:03 -0800

    new places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,9 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
+
+router.get('/', (req, res) => {
+    res.render('places/index', { places })
+})
 
 router.get('/', (req, res) => {
     res.render('GET /places')
@@ -10,7 +15,21 @@ router.get('/new', (req, res) => {
     res.render('places/new')
   })
   
-
+  router.post('/', (req, res) => {
+   // console.log(req.body)
+    if (!req.body.pic) {
+        req.body.pic = 'http://placekitten.com/400/400'
+      }
+      if (!req.body.city) {
+        req.body.city = 'Anytown'
+      }
+      if (!req.body.state) {
+        req.body.state = 'USA'
+      }
+      places.push(req.body)
+    res.redirect('/places')
+  })
+  
 
 router.get('/places', (req, res) => {
     let places = [{
